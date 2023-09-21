729c3586fcf31cc2b173f7145e5c518fd9e4b551
swhyeon98 <smreo75@gmail.com>
2023-09-21 18:42:39 +0900

    refactor: 'v1'이라는 이름이 누락된 클래스에 이름 추가

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class MemberSaveController implements ControllerV1 {
+public class MemberSaveControllerV1 implements ControllerV1 {
 
     MemberRepository memberRepository = MemberRepository.getInstance();
 
