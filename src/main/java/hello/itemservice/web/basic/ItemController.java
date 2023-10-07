68412948801313a3d3d1361cb948110f930fa8d3
swhyeon98 <smreo75@gmail.com>
2023-10-07 22:36:56 +0900

    상품 등록 폼

--- src/main/java/hello/itemservice/web/basic/ItemController.java
+++ src/main/java/hello/itemservice/web/basic/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -30,8 +31,17 @@ public class ItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addFrom() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
     }
 
     /**
