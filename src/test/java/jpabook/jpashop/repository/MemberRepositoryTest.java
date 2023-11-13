d227991fb6f9dcc8bcd1c688929367b3be112851
swhyeon98 <smreo75@gmail.com>
2023-11-13 18:57:12 +0900

    엔티티 클래스 개발 1

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -1,7 +1,5 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.domain.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
