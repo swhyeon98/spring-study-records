2cf8278c9f057a36b3faaccee0da46613af5c63a
swhyeon98 <smreo75@gmail.com>
2023-11-20 22:48:20 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,4 +32,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity -= stockQuantity;
+        if (stockQuantity < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = stockQuantity;
+    }
 }
