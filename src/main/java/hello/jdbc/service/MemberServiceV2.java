20f06c47308bb799e819935e86dc1871ab85b10b
swhyeon98 <smreo75@gmail.com>
2024-03-07 23:28:29 +0900

    refactor(MemberServiceV2): 메서드 분리

--- src/main/java/hello/jdbc/service/MemberServiceV2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -24,23 +24,17 @@ public class MemberServiceV2 {
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         Connection con = dataSource.getConnection();
         try {
-            con.setAutoCommit(false); // 트랜잭션 시작
-            //  비즈니스 로직
+            con.setAutoCommit(false);// 트랜잭션 시작
+            // 비즈니스 로직
             bizLogic(con, fromId, toId, money);
-            con.commit();   // 성공시 커밋
+            con.commit(); // 성공시 커밋
         } catch (Exception e) {
             con.rollback(); // 실패시 롤백
             throw new IllegalStateException(e);
         } finally {
-            if (con != null) {
-                try {
-                    con.setAutoCommit(true);
-                    con.close();
-                } catch (Exception e) {
-                    log.info("error", e);
-                }
-            }
+            release(con);
         }
+
     }
 
     private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
@@ -52,9 +46,20 @@ public class MemberServiceV2 {
         memberRepository.update(con, toId, toMember.getMoney() + money);
     }
 
-    private static void validation(Member member) {
-        if (member.getMemberId().equals("ex")) {
-            throw new IllegalStateException("이체 중 예외 발생");
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
         }
     }
 }
